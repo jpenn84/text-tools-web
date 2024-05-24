232e7f5fe49c2a72f1e96aea921175c9923d0129
Joshua Penn <joshualeepenn@gmail.com>
2024-05-24 14:13:39 -0400

    #11 Add a Clear All button

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,6 @@
+// Logic to show alert only if clearing text before copying
+var textCopied = false;
+
 function copyOutputText() {
   // Get the text field
   var copyText = document.getElementById("outputText");
@@ -11,6 +14,19 @@ function copyOutputText() {
   // Copy the text inside the text field
   navigator.clipboard.writeText(copyText.value);
 
+  // allow clearText method to suppress confirmation dialog
+  textCopied = true;
+
   // Alert the copied text
   // alert("Copied the text: " + copyText.value);
+}
+
+function clearText() {
+  // If text exists in either text box and has not yet been copied, show confirmation dialog
+  if (!textCopied && ($("#inputText").val().trim().length > 0 || $("#outputText").val().trim().length > 0))
+    if (!confirm("Clear all?")) return;
+  $("#inputText").val("");
+  $("#outputText").val("");
+  document.getElementById("outputText").value = "";
+  textCopied = false;
 }
